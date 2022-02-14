77ce45593f9fdefb2218b93812963bdc200d15b5
Javier Candales <javier_candales@yahoo.com.ar>
2022-02-13 22:41:29 -0300

    Document card.h

--- include/card.h
+++ include/card.h
@@ -12,11 +12,59 @@ typedef struct {
 	int value, type, power, envido;
 } Card;
 
+/**
+ * @brief Create a new card.
+ * 
+ * @param value 1 to 7 and 10 to 12.
+ * @param type ORO, COPAS, ESPADAS, BASTOS.
+ * @param pow 0 (4 OCEB) to 13 (1 E)
+ * @param envido envido points = values, 10 11 12 = 0
+ * @return Card* card pointer.
+ */
 Card *card_new(int value, int type, int pow, int envido);
+
+/**
+ * @brief Generate long name string. ex:
+ * - 4B ->  "el 4 de bastos".
+ * - 10C -> "la sota de copas".
+ * - 1E ->  "el ancho de espadas".
+ * 
+ * Note: string requires destroy after.
+ * 
+ * @param card card instance.
+ * @return char* the string name.
+ */
 char *card_long_name(Card *card);
+
+/**
+ * @brief Generate short name string.
+ * Ex: 4B, 6C, 10O
+ * 
+ * Note: string requires destroy after.
+ * 
+ * @param card card instance.
+ * @return char* the string name.
+ */
 char *card_short_name(Card *card);
 
+/**
+ * @brief Draw normal card on window at position x, y
+ * 
+ * @param wnd WINDOW to draw the card.
+ * @param card card instace.
+ * @param x x position
+ * @param y y position
+ */
 void card_draw(WINDOW *wnd, Card *card, int x, int y);
+
+/**
+ * @brief Draw small card on window at position x, y
+ * 
+ * @param wnd WINDOW to draw the card.
+ * @param card card instace.
+ * @param x x position
+ * @param y y position
+ */
 void card_draw_small(WINDOW *wnd, Card *card, int x, int y);
 
 #endif
